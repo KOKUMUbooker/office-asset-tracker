634c3172fd83344bbf7b408dfe67b443ea506d87
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-16 11:28:03 +0300

    Add sql server set up script

---
+++ sql-server-set-up.sql
@@ -0,0 +1,40 @@
+﻿/* Create DB */
+IF DB_ID('AssetTrackerDB') IS NULL
+BEGIN
+    CREATE DATABASE AssetTrackerDB;
+END
+GO
+
+/* Switch context to the current DB */
+USE AssetTrackerDB;
+GO
+
+/* Create database tables in dbo schema with their relationships & constraints  */
+CREATE TABLE dbo.[Category] (
+    Id INT IDENTITY PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL
+);
+GO
+
+CREATE TABLE dbo.[Staff] (
+    Id INT IDENTITY PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL
+);
+GO
+
+CREATE TABLE dbo.Asset (
+    Id INT IDENTITY PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    CategoryId INT NOT NULL,
+    Status NVARCHAR(50) NOT NULL,
+    AssignedToStaffId INT NULL,
+
+    CONSTRAINT FK_Asset_Category
+        FOREIGN KEY (CategoryId)
+        REFERENCES dbo.Category(Id),
+
+    CONSTRAINT FK_Asset_User
+        FOREIGN KEY (AssignedToStaffId)
+        REFERENCES dbo.[Staff](Id)
+);
+GO
