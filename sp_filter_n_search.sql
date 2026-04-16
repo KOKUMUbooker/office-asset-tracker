31ef341c771d1694f44cc116054012d74c784e9d
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-16 16:24:18 +0300

    Add stored procedures for filter & search

---
+++ sp_filter_n_search.sql
@@ -0,0 +1,46 @@
+/* Get assets by category id */
+CREATE PROCEDURE sp_Asset_GetByCategory
+( @CategoryId INT )
+AS
+BEGIN
+	SELECT * FROM dbo.Asset
+	WHERE CategoryId=@CategoryId
+END
+GO;
+
+/* Get assets by status */
+CREATE PROCEDURE sp_Asset_GetByStatus
+( @Status NVARCHAR(50) )
+AS
+BEGIN
+	SELECT * FROM dbo.Asset
+	WHERE Status=@Status
+END
+GO;
+
+/* Get assets by name */
+CREATE PROCEDURE sp_Asset_SearchByName
+( @Name NVARCHAR(50) )
+AS
+BEGIN
+	SELECT * FROM dbo.Asset
+	WHERE Name LIKE '%' + @Name + '%'
+END
+GO;
+
+/* Get assets by a combination of CategoryId, AssignedToStaffId and Status */
+CREATE PROCEDURE sp_Asset_Filter
+(
+ @CategoryId INT = NULL,
+ @AssignedToStaffId INT = NULL,
+ @Status NVARCHAR(50) = NULL
+)
+AS
+BEGIN
+	SELECT * FROM dbo.Asset
+	WHERE
+		(CategoryId = @CategoryId OR @CategoryId IS NULL) AND
+		(AssignedToStaffId = @AssignedToStaffId OR @AssignedToStaffId IS NULL) AND
+		(Status = @Status OR @Status IS NULL)
+END
+GO;
