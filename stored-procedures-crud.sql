ea0e5e130626ddd174a160f426eed6277553ee1c
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-16 14:30:34 +0300

    SQL script file rename & add crud SPs for staff and asset

--- stored-procedures-crud.sql
+++ stored-procedures-crud.sql
@@ -1,5 +1,4 @@
 /* Category stored procedures */
-/* ========================= A) CRUD ================================= */
 /* ==================== CATEGORIES ==============================*/
 /* 1. Get all categories */
 CREATE PROCEDURE sp_Category_GetAll
@@ -62,7 +61,7 @@ BEGIN
 END;
 GO;
 
-/*2.  Get staff by Id */
+/* 2.  Get staff by Id */
 CREATE PROCEDURE sp_Staff_GetById 
 ( @Id INT)
 AS
@@ -71,7 +70,7 @@ BEGIN
 END;
 GO;
 
-/* 3. Create category */
+/* 3. Create staff */
 CREATE PROCEDURE sp_Staff_Insert
 ( @StaffName NVARCHAR(100) )
 AS
@@ -81,26 +80,83 @@ BEGIN
 END;
 GO;
 
-/* 4. Update category */
-CREATE PROCEDURE sp_Category_Update 
+/* 4. Update staff */
+CREATE PROCEDURE sp_Staff_Update 
 ( 
 	@Id INTEGER, 
-	@NewCategoryName NVARCHAR(100)
+	@NewStaffName NVARCHAR(100)
 )
 AS
 BEGIN
-	UPDATE dbo.Category
-	SET Name=@NewCategoryName
+	UPDATE dbo.Staff
+	SET Name=@NewStaffName
 	WHERE Id=@Id
 END;
 GO;
 
-/* 5. Delete category */
-CREATE PROCEDURE sp_Category_Delete 
+/* 5. Delete staff */
+CREATE PROCEDURE sp_Staff_Delete 
 ( @Id INTEGER )
 AS
 BEGIN
 	DELETE 
-	FROM dbo.Category
+	FROM dbo.Staff
+	WHERE Id=@Id
+END;
+GO;
+
+/* ==================== ASSET ==============================*/
+/* 1. Get all assets */
+CREATE PROCEDURE sp_Asset_GetAll
+AS
+BEGIN
+	SELECT * FROM dbo.Asset;
+END;
+GO;
+
+/* 2. Get asset by Id */
+CREATE PROCEDURE sp_Asset_GetById 
+( @Id INT)
+AS
+BEGIN
+	SELECT * FROM dbo.Asset WHERE Id=@Id;
+END;
+GO;
+
+/* 3. Create asset */
+CREATE PROCEDURE sp_Asset_Insert
+(
+ @Name NVARCHAR(100),
+ @CategoryId INTEGER,
+ @Status NVARCHAR(50)
+)
+AS
+BEGIN
+	INSERT INTO dbo.Asset (Name, CategoryId, Status)
+	VALUES (@StaffName, @CategoryId, @Status)
+END;
+GO;
+
+/* 4. Update asset */
+CREATE PROCEDURE sp_Asset_Update 
+( 
+	@Id INTEGER, 
+	@NewName NVARCHAR(100)
+)
+AS
+BEGIN
+	UPDATE dbo.Asset
+	SET Name=@NewName
+	WHERE Id=@Id
+END;
+GO;
+
+/* 5. Delete asset */
+CREATE PROCEDURE sp_Asset_Delete 
+( @Id INTEGER )
+AS
+BEGIN
+	DELETE 
+	FROM dbo.Asset
 	WHERE Id=@Id
 END;
