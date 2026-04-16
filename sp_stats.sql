62236f0e16c74dec3e75f3a0a6a8c18c93bc061a
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-16 17:27:51 +0300

    Add stored procedures for getting stat data on assets

---
+++ sp_stats.sql
@@ -0,0 +1,34 @@
+/* Get asset counts based on status */
+CREATE PROCEDURE sp_Asset_CountByStatus
+AS
+BEGIN
+	SELECT Status, COUNT(Id) AS Count
+	FROM dbo.Asset
+	GROUP BY Status
+END
+GO;
+
+/* Get asset counts based on category */
+CREATE PROCEDURE sp_Asset_CountByCategory
+AS
+BEGIN
+	SELECT c.Name, COUNT(a.Id) AS Count
+	FROM dbo.Asset a
+	LEFT JOIN dbo.Category c
+		ON a.CategoryId = c.Id
+	GROUP BY c.Name
+END
+GO;
+
+/* Get assets stats totalCount,AvailableCount,AssignedCount and DamagedCount */
+CREATE PROCEDURE sp_Asset_StatsOverview
+AS
+BEGIN
+	SELECT 
+    COUNT(*) AS TotalAssets,
+    SUM(CASE WHEN Status = 'Available' THEN 1 ELSE 0 END) AS AvailableCount,
+    SUM(CASE WHEN Status = 'Assigned' THEN 1 ELSE 0 END) AS AssignedCount,
+    SUM(CASE WHEN Status = 'Damaged' THEN 1 ELSE 0 END) AS DamagedCount
+	FROM dbo.Asset;
+END
+GO;
