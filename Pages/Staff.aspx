b361780c1374b652c0943677cd74b653f4cd7e65
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-17 17:37:35 +0300

    Hook up data fetched from StaffRepo to the aspx files

--- Pages/Staff.aspx
+++ Pages/Staff.aspx
@@ -1,5 +1,10 @@
 ﻿<%@ Page Title="Staff" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Staff.aspx.cs" Inherits="Staff" %>
 
 <asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
-   
+   <asp:GridView ID="gvStaff" runat="server" AutoGenerateColumns="false">
+        <Columns>
+            <asp:BoundField DataField="Id" HeaderText="ID" />
+            <asp:BoundField DataField="Name" HeaderText="Category Name" />
+        </Columns>
+    </asp:GridView>
 </asp:Content>
--- Pages/Staff.aspx.cs
+++ Pages/Staff.aspx.cs
@@ -4,11 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using office_asset_tracker.DAL;
 
 public partial class Staff : System.Web.UI.Page
 {
+    private StaffRepository _repo;
+
+    public Staff()
+    {
+        _repo = new StaffRepository();
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            var categories = _repo.GetStaffMembers();
 
+            gvStaff.DataSource = categories;
+            gvStaff.DataBind();
+        }
     }
 }
