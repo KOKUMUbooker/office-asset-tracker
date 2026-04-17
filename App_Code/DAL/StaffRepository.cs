ff32a338fc6f7bf6e5d893d58cf8f1f6023495bd
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-17 17:33:18 +0300

    Implement Staff Data access layer class

---
+++ App_Code/DAL/StaffRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using office_asset_tracker.Models;
+
+namespace office_asset_tracker.DAL
+{
+    /// <summary>
+    /// Summary description for StaffRepository
+    /// </summary>
+    public class StaffRepository : BaseRepository
+    {
+        public List<Staff> GetStaffMembers()
+        {
+            var staff = new List<Staff>();
+
+            using (SqlConnection conn = new SqlConnection(ConnString))
+            using (SqlCommand cmd = new SqlCommand("sp_Category_GetAll", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                conn.Open();
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        staff.Add(new Staff
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Name = reader["Name"].ToString(),
+                        });
+                    }
+                }
+            }
+
+            return staff;
+        }
+    }
+}
