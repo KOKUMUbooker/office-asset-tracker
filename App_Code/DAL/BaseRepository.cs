af2b2a4fc1c35b99d75c9bd304e0cf8d300f58df
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-17 17:34:58 +0300

    Implement BaseRepo class for reading DB Connection string from webconfig

---
+++ App_Code/DAL/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace office_asset_tracker.DAL
+{
+    /// <summary>
+    /// Summary description for BaseRepository
+    /// </summary>
+    public class BaseRepository
+    {
+        protected readonly string ConnString;
+
+        public BaseRepository()
+        {
+            ConnString = ConfigurationManager
+                .ConnectionStrings["DefaultConnection"]
+                .ConnectionString;
+        }
+    }
+}
