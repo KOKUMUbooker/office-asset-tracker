2b51fc45b07755f215288826ca82af317c4217b8
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-18 13:10:50 +0300

    Implement form for asset creation

--- Pages/Forms/AssetForm.aspx
+++ Pages/Forms/AssetForm.aspx
@@ -1,5 +1,31 @@
-﻿<%@ Page Title="Asset Form" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="AssetForm.aspx.cs" Inherits="Pages_Forms_AssetForm" %>
+﻿<%@ Page Title="Asset Form" Language="C#" MasterPageFile="~/Site.Master"
+AutoEventWireup="true" CodeFile="AssetForm.aspx.cs"
+Inherits="Pages_Forms_AssetForm" %>
 
 <asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
-    
+  <h1>Add new asset</h1>
+  <asp:Label ID="AssetNameLabel" runat="server" Text="Enter Asset Name: " />
+  <asp:TextBox ID="AssetName" runat="server" />
+  <br /><br />
+
+  <asp:Label
+    ID="AssetCategoryLabel"
+    runat="server"
+    Text="Specify Category : "
+  ></asp:Label>
+   <asp:DropDownList ID="AssetCategory" runat="server" CssClass="select">
+   </asp:DropDownList>
+    <br /><br />
+
+   <asp:Label
+    ID="AssetStatusLabel"
+    runat="server"
+    Text="Specify Status : "
+  ></asp:Label>
+   <asp:DropDownList ID="AssetStatus" runat="server" CssClass="select">
+   </asp:DropDownList>
+    <br /><br />
+
+   <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="BtnSubmit_Click" CssClass="btn btn-contained"/>
+   <br /><br />
 </asp:Content>
--- Pages/Forms/AssetForm.aspx.cs
+++ Pages/Forms/AssetForm.aspx.cs
@@ -1,14 +1,56 @@
 ﻿using System;
+using System.Data;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using office_asset_tracker.DAL;
+using office_asset_tracker.Models;
 
 public partial class Pages_Forms_AssetForm : System.Web.UI.Page
 {
+    private CategoryRepository _categoryRepo;
+
+    public Pages_Forms_AssetForm()
+    {
+        _categoryRepo = new CategoryRepository();
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            BindValuesToCategoryDropdown();
+            BindValuesToStatusDropdown();
+        }
+    }
 
+    protected void BtnSubmit_Click(object sender, EventArgs e)
+    {
+       
+    }
+
+    private void BindValuesToCategoryDropdown() 
+    {
+        var categories = _categoryRepo.GetCategories();
+
+        AssetCategory.DataSource = categories;
+        AssetCategory.DataTextField = "Name"; // what user sees
+        AssetCategory.DataValueField = "Id";  // actual value
+
+        AssetCategory.DataBind();
+        AssetCategory.Items.Insert(0, new ListItem("-- Select Category --", ""));
+    }
+
+    private void BindValuesToStatusDropdown()
+    {
+        AssetStatus.Items.Add(new ListItem("-- Select Status --", ""));
+        AssetStatus.Items.Add("Available");
+        AssetStatus.Items.Add("Assigned");
+        AssetStatus.Items.Add("Damaged");
+
+        AssetCategory.DataBind();
+        AssetStatus.SelectedValue = "Available";
     }
 }
